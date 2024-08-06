a7119204af04010c232c99e86756898ff021a46b
Ollie Copping <oliver.copping@diamond.ac.uk>
2024-08-06 08:49:45 +0100

    Change crash report extension based on MacOS version

--- ci_core_dumper/osx.py
+++ ci_core_dumper/osx.py
@@ -4,11 +4,23 @@
 
 import time
 import os
+import platform
 from glob import glob
 
 from . import CommonDumper
 
 class DarwinDumper(CommonDumper):
+
+    def __init__(self, args):
+        super().__init__(args)
+
+        v, _, _ = platform.mac_ver()
+        v = float('.'.join(v.split('.')[:2]))
+        if v >= 12.0:
+            self.crash_ext = "ips"
+        else:
+            self.crash_ext = "crash"
+
     def install(self):
         pass
     def uninstall(self):
@@ -19,7 +31,7 @@ class DarwinDumper(CommonDumper):
         # so we just wait a while and hope for the best
         time.sleep(10)
 
-        for path in ('~/Library/Logs/DiagnosticReports/*.crash', '~/Library/Logs/CrashReporter/*.crash'):
+        for path in ('~/Library/Logs/DiagnosticReports/*.diag', '~/Library/Logs/DiagnosticReports/*.dpsub', '~/Library/Logs/CrashReporter/*.{0}'.format(self.crash_ext)):
             for report in glob(os.path.expanduser(path)):
                 self.error(report)
                 self.catfile(report)
